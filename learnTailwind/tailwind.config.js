4a8ccba98a19b173485b3cdb5fa82aa68bc662be
Misty <sultanamisty94@gmail.com>
2023-10-30 15:49:55 +0600

    initial commit

---
+++ learnTailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode:"media",
+  
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
+
